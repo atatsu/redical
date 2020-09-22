0ea77ff5315808b8817768ad6ea1883d03146c71
atatsu <nathan.lundquist@gmail.com>
2020-09-21 23:49:07 -0500

    Updated the `BaseMixin` class to only define the `execute` method if the type checker is running.

--- redical/command/base.py
+++ redical/command/base.py
@@ -1,6 +1,7 @@
-from typing import Any, AnyStr, Awaitable
+from typing import Any, AnyStr, Awaitable, TYPE_CHECKING
 
 
 class BaseMixin:
-	def execute(self, command: AnyStr, *args: Any, encoding: str = 'utf-8', **kwargs: Any) -> Awaitable[Any]:
-		pass
+	if TYPE_CHECKING:
+		def execute(self, command: AnyStr, *args: Any, encoding: str = 'utf-8', **kwargs: Any) -> Awaitable[Any]:
+			pass
